7f402f1c3a3cb279408723db78ea232c426bac84
pc <pc@DESKTOP-78M62IA>
2024-10-24 20:15:03 +0300

    Add project files.

---
+++ IlkSınıfımızıOluşturuyoruz/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Person
+{
+    // Properties (özellikler)
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public DateTime DogumTarihi { get; set; }
+
+    // Constructor (yapıcı metod)
+    public Person(string ad, string soyad, DateTime dogumTarihi)
+    {
+        Ad = ad;
+        Soyad = soyad;
+        DogumTarihi = dogumTarihi;
+    }
+
+    // Method (metod)
+    public void BilgileriYazdir()
+    {
+        Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}, Doğum Tarihi: {DogumTarihi.ToShortDateString()}");
+    }
+}
---
+++ IlkSınıfımızıOluşturuyoruz/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Öğrenci nesneleri oluşturma
+        Person ogrenci1 = new Person("Ali", "Yılmaz", new DateTime(2010, 5, 15));
+        Person ogrenci2 = new Person("Ayşe", "Kara", new DateTime(2011, 8, 22));
+
+        // Öğretmen nesneleri oluşturma
+        Person ogretmen1 = new Person("Mehmet", "Demir", new DateTime(1985, 3, 30));
+        Person ogretmen2 = new Person("Fatma", "Aydın", new DateTime(1990, 12, 1));
+
+        // Bilgileri konsola yazdırma
+        Console.WriteLine("Öğrenciler:");
+        ogrenci1.BilgileriYazdir();
+        ogrenci2.BilgileriYazdir();
+
+        Console.WriteLine("\nÖğretmenler:");
+        ogretmen1.BilgileriYazdir();
+        ogretmen2.BilgileriYazdir();
+    }
+}
